187800fef6d4163f9d2d8c0e6d9034cd2e2e038e
luismiguel.saez <luismiguel.saez@teamcmp.com>
2021-08-12 16:08:19 +0200

    Added user-data

--- aws/cloudwatch/alarms/user-data.sh
+++ aws/cloudwatch/alarms/user-data.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+PKG_URL="https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb"
+
+curl -Ls ${PKG_URL} -o/tmp/amazon-cloudwatch-agent.deb && dpkg -i /tmp/amazon-cloudwatch-agent.deb
+
+apt-get -y update
+apt-get -y install apache2
+systemctl start apache2
+
+cat << EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.toml
+
+EOF
