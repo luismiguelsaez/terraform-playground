746a182bb5c68ad0b515d74d13863c5eb19228f1
luismiguel.saez <luismiguel.saez@teamcmp.com>
2021-08-12 16:22:39 +0200

    Added install commands

--- aws/cloudwatch/alarms/user-data.sh
+++ aws/cloudwatch/alarms/user-data.sh
@@ -5,9 +5,28 @@ PKG_URL="https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/ama
 curl -Ls ${PKG_URL} -o/tmp/amazon-cloudwatch-agent.deb && dpkg -i /tmp/amazon-cloudwatch-agent.deb
 
 apt-get -y update
-apt-get -y install apache2
+apt-get -y install apache2 python2
 systemctl start apache2
 
-cat << EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.toml
+CUR_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 
+cat << EOF > /etc/awslogs.json
+{
+   "logs":{
+      "logs_collected":{
+         "files":{
+            "collect_list":[
+               {
+                  "file_path":"/var/log/apache2/*.log",
+                  "log_group_name":"apps/web/http",
+                  "log_stream_name":"apache"
+               }
+            ]
+         }
+      }
+   }
+}
 EOF
+
+curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+python2 ./awslogs-agent-setup.py -r ${CUR_REGION} -c /etc/awslogs.json -p /usr/bin/python2
