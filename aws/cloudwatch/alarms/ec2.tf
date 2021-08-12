ef2123f6efda0f5937d120dbbd1a6a66652ced8d
luismiguel.saez <luismiguel.saez@teamcmp.com>
2021-08-12 15:46:47 +0200

    Modified to public subnet

--- aws/cloudwatch/alarms/ec2.tf
+++ aws/cloudwatch/alarms/ec2.tf
@@ -1,6 +1,10 @@
 module "web_instance" {
   source         = "../../ec2/public-instance"
   vpc_id         = "vpc-0923724dbb0eefcbd"
-  subnet_id      = "subnet-0e8a75f17e497d808"
+  subnet_id      = "subnet-0345b82af5d72c35d" # Public subnet
   user_data_path = "./user-data.sh"
+}
+
+output "instance_public_ip" {
+  value = module.web_instance.instance_public_ip
 }
